1d23e4b3cb58b3f9dafb2e9b63ece1a7b7e3c084
MAC <mac@Prabhdeeps-MacBook-Pro.local>
2025-12-26 22:39:34 +0530

    appwrite auth

--- appwriteConfig.ts
+++ appwriteConfig.ts
@@ -2,8 +2,8 @@
 import { Account, Client } from "appwrite";
 
 const client = new Client()
-  .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_URL!) // your Appwrite endpoint
-  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!); // replace with your project ID
+  .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!) // your Appwrite endpoint
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_NAME!); // replace with your project ID
 
 export const account = new Account(client);
 export { client };
